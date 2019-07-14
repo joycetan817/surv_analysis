a0596a8e0ca88c92761329d838daf93716ff67bc
Jiayi Tan <jitan@coh.org>
2019-07-14 10:16:45 -0700

    Update function_test.R
    add annotation

--- function_test.R
+++ function_test.R
@@ -1,4 +1,4 @@
-# This script is for survaval analysis of subtype samples with defined function
+# This script is for survaval analysis of subtype samples using defined function
 # Jiayi Tan
 # 06/28/2019
 
@@ -15,7 +15,7 @@ cat("Loading METABRIC expression data...\n")
 st = Sys.time()
 meta_expr<-readRDS("metabric_expr_ilid.RDS")
 print(Sys.time()-st)
-data.meta<-t(meta_expr)
+data.meta<-t(meta_expr) #transpose data list according to gene.fu function format 
 
 cat("Loading clinical data...\n")
 st = Sys.time()
@@ -23,8 +23,8 @@ clin_info<-readRDS("merge_clin_info_manual_checked.RDS")
 print(Sys.time()-st)
 
 
-subtype_clin=sub_clin(clin = clin_info, subtype = "Her2", coloi = "Pam50Subtype")
-subtype = "Her2"
+subtype_clin=sub_clin(clin = clin_info, subtype = "Basal", coloi = "Pam50Subtype")
+subtype = "Basal"
 
 
 cat("Loading METABRIC annotation data...\n")
@@ -41,7 +41,7 @@ TRM_sig<-TRM_sig[1:2]
 colnames(TRM_sig)[1]="NCBI.gene.symbol"
 colnames(TRM_sig)[2]="coefficient"
 TRM_sig$EntrezGene.ID<-annot.meta$EntrezGene.ID[match(TRM_sig$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
-TRM_sig$probe<-annot.meta$probe[match(TRM_sig$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
+TRM_sig$probe<-annot.meta$probe[match(TRM_sig$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)] # probe is necessary
 
 cat("Run TRM gene sig.score in TNBC/Basal samples\n")
 TRM.score <- sig.score(x=TRM_sig, data=data.meta, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
@@ -55,21 +55,23 @@ colnames(TRM_score)[2]="sig.score"
 sub_score<-subset(TRM_score, TRM_score$patient_id %in% subtype_clin$pid)
 sub_score<-sub_score[order(-sub_score$sig.score),]
 library(dplyr)
+
+cat("Select patient samples by 25/75 cutoff of TRM sig.score")
 top_25<-subset(sub_score, sig.score > quantile(sig.score, prob = 1 - 25/100))
-bot_75<-subset(sub_score, sig.score < top_25$sig.score)
+bot_75<-subset(sub_score, sig.score < top_25$sig.score) #select bottom 75% data
 top_25$score="25%"
 bot_75$score="75%"
 sub_score<-rbind(top_25,bot_75)
 
-
+cat("Extract survival information from clinical data")
 sub_score$OS<-subtype_clin$T[match(subtype_clin$pid, sub_score$patient_id)]
 sub_score$os_status<-subtype_clin$DeathBreast[match(subtype_clin$pid, sub_score$patient_id)]
-subtype_clin$DFS<-ifelse(subtype_clin$TLR>subtype_clin$TDR,subtype_clin$TDR,subtype_clin$TLR)
+subtype_clin$DFS<-ifelse(subtype_clin$TLR>subtype_clin$TDR,subtype_clin$TDR,subtype_clin$TLR) # dfs includes local relapse and distant replase
 subtype_clin$dfs_status<-ifelse(c(subtype_clin$LR==0 & subtype_clin$DR==0),0,1) 
 sub_score$DFS<-subtype_clin$DFS[match(sub_score$patient_id,subtype_clin$pid)]
 sub_score$dfs_status<-subtype_clin$dfs_status[match(sub_score$patient_id,subtype_clin$pid)]
 
-
+cat("Save TRM sig.score with survival data")
 filename<- paste("c:/Users/jitan/Documents/", subtype, ".xlsx", sep = "")
 library(xlsx)
 write.xlsx(sub_score, filename)
@@ -82,7 +84,7 @@ hist<-ggplot(sub_score, aes(x=sig.score)) + geom_histogram(color="darkblue", fil
   labs(title="", x="sig.score", y = "Count")+
   theme_classic()
 
-filename<- paste("c:/Users/jitan/Documents/hist_", subtype, ".jpeg", sep = "")
+filename<- paste("c:/Users/jitan/Documents/hist_", subtype, ".jpeg", sep = "") # save files in different subtypes
 
 ggsave(hist, file= filename)
 
@@ -120,4 +122,3 @@ dfs_plot <-ggsurvplot(dfs_fit,
 filename<- paste("c:/Users/jitan/Documents/", subtype, "_dfs.jpeg", sep = "")
 
 ggsave(file=filename, print(dfs_plot))
-
