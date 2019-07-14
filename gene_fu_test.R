86b12b8b8c036a89d4c5896a8c10b9acc75664aa
Jiayi Tan <jitan@coh.org>
2019-07-14 09:52:38 -0700

    Update gene_fu_test.R
    Generate survival plot in TNBC/Basal samples

--- gene_fu_test.R
+++ gene_fu_test.R
@@ -2,16 +2,7 @@
 # Jiayi Tan
 # 06/24/2019
 
-#rm(list = ls(all.names = TRUE))
-
-test_func = function (clin, subtype, coloi) {
-#	clin_oi = subset(clin, Pam50Subtype == subtype)
-	temp_mask = clin[,coloi] == subtype
-	print(head(temp_mask))
-	clin_oi = clin[temp_mask,]
-	print(dim(clin_oi))
-	return(clin_oi)
-}
+rm(list = ls(all.names = TRUE))
 
 
 cat("Loading genefu library...\n")
@@ -27,25 +18,15 @@ st = Sys.time()
 clin_info<-readRDS("merge_clin_info_manual_checked.RDS")
 print(Sys.time()-st)
 
-used_clin = test_func(clin = clin_info, subtype = "LumA", coloi = "Pam50Subtype")
-stop("JUST STOP!!!!!!!")
-if (FALSE) {
 cat("Select TNBC/Basal samples from clinical data frame\n")
 Basal_info<-subset(clin_info, Pam50Subtype =="Basal")
 TNBC_info<-subset(clin_info, er_status =="-"& her2_status =="-"& pr_status =="-")
 TNBC_Basal<-rbind(Basal_info,TNBC_info)
 TNBC_Basal<-unique(TNBC_Basal)
-IDC_info<-subset(clin_info, oncotree_code =="IDC")
-library(dplyr)
-TNBC_Basal.IDC<-intersect(TNBC_Basal,IDC_info)
-TNBC.Basal<-intersect(TNBC_info,Basal_info)
 
-cat("Generate expression data from TNBC/Basal/IDC samples\n")
-data.meta_TNBC<-subset(data.meta,rownames(data.meta) %in% TNBC_info$pid)
-data.meta_Basal<-subset(data.meta,rownames(data.meta) %in% Basal_info$pid)
+cat("Generate expression data from TNBC/Basal samples\n")
+
 data.meta_T_B<-subset(data.meta,rownames(data.meta) %in% TNBC_Basal$pid)
-data.meta_T.B<-subset(data.meta,rownames(data.meta) %in% TNBC.Basal$pid)
-data.meta_T_B.IDC<-subset(data.meta,rownames(data.meta) %in% TNBC_Basal.IDC$pid)
 
 
 cat("Loading METABRIC annotation data...\n")
@@ -66,38 +47,55 @@ TRM_sig$probe<-annot.meta$probe[match(TRM_sig$NCBI.gene.symbol, annot.meta$NCBI.
 
 cat("Run TRM gene sig.score in TNBC/Basal samples\n")
 TRM.score <- sig.score(x=TRM_sig, data=data.meta_T_B, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
-TRM_T.score <- sig.score(x=TRM_sig, data=data.meta_TNBC, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
 
-TRM_B.score <- sig.score(x=TRM_sig, data=data.meta_Basal, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
-
-TRM_TB.score <- sig.score(x=TRM_sig, data=data.meta_T.B, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
-
-TRM_IDC.score <- sig.score(x=TRM_sig, data=data.meta_T_B.IDC, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
 
 
 TRM_score<- as.data.frame (TRM.score$score)
 library(tibble)
 TRM_score<-rownames_to_column(TRM_score,var ="patient_id")
 colnames(TRM_score)[2]="sig.score"
-
-test<- as.data.frame (TRM.score$score)
-library(tibble)
-test1<-rownames_to_column(test, var ="patient_id")
-test2<-test1[order(-test1$sig.score),]
+TRM_score<-TRM_score[order(-TRM_score$sig.score),]
 library(dplyr)
-top_25<-test2 %>% top_n(93)
-bot_75<-test2 %>% top_n(-279)
+top_25<-TRM_score %>% top_n(99)
+bot_75<-TRM_score %>% top_n(-296)
 top_25$score="25%"
 bot_75$score="75%"
-TNBC_Basal1<-rbind(top_25,bot_75)
-TNBC_Basal1$os<-TNBC_Basal$os_time[match(TNBC_Basal$pid, TNBC_Basal1$patient_id)]
-TNBC_Basal1$status<-TNBC_Basal$os[match(TNBC_Basal$pid, TNBC_Basal1$patient_id)]
-colnames(TNBC_Basal1)[4]="time"
-TNBC_Basal1$status<-c("LIVING"=1,"DECEASED"=2)
-fit <- survfit(Surv(time, status) ~ score, data = TNBC_Basal1)
-print(fit)
-summary(fit)$table
+TRM.T_B<-rbind(top_25,bot_75)
+
+cat("Extract survival information from clinical data")
+TRM.T_B$OS<-TNBC_Basal$T[match(TNBC_Basal$pid, TRM.T_B$patient_id)]
+TRM.T_B$os_status<-TNBC_Basal$DeathBreast[match(TNBC_Basal$pid, TRM.T_B$patient_id)]
+TNBC_Basal$DFS<-ifelse(TNBC_Basal$TLR>TNBC_Basal$TDR,TNBC_Basal$TDR,TNBC_Basal$TLR)
+TNBC_Basal$dfs_status<-ifelse(c(TNBC_Basal$LR==0 & TNBC_Basal$DR==0),0,1) 
+TRM.T_B$DFS<-TNBC_Basal$DFS[match(TRM.T_B$patient_id,TNBC_Basal$pid)]
+TRM.T_B$dfs_status<-TNBC_Basal$dfs_status[match(TRM.T_B$patient_id,TNBC_Basal$pid)]
+
+cat("Generate survival plot\n")
+
+library("survival")
+library("survminer")
+
+TRM.T_B$os_status<-as.numeric(TRM.T_B$os_status)
 
 
-library(ggplot2)
-ggplot(TRM.score, aes(x=sig.score)) + geom_histogram()}
+os_fit <- survfit(Surv(OS, os_status) ~ score, data = TRM.T_B)
+dfs_fit <- survfit(Surv(DFS, dfs_status) ~ score, data = TRM.T_B)
+
+os_plot<-ggsurvplot(os_fit,
+          pval = TRUE, conf.int = FALSE, ylab = "Overall survival",
+          risk.table = TRUE, # Add risk table
+          risk.table.col = "strata", # Change risk table color by groups
+          linetype = "strata", # Change line type by groups
+          surv.median.line = "hv", # Specify median survival
+          palette = c("#E7B800", "#2E9FDF"))
+
+dfs_plot <-ggsurvplot(dfs_fit,
+          pval = TRUE, conf.int = FALSE, ylab = "Disease-free survival",
+          risk.table = TRUE, # Add risk table
+          risk.table.col = "strata", # Change risk table color by groups
+          linetype = "strata", # Change line type by groups
+          surv.median.line = "hv", # Specify median survival
+          palette = c("#E7B800", "#2E9FDF"))
+
+ggsave(print(os_plot), file= "c:/Users/jitan/Documents/TRM.T_B_os.jpeg")
+ggsave(print(dfs_plot), file= "c:/Users/jitan/Documents/TRM.T_B_dfs.jpeg")
