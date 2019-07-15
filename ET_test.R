f4d5666503f90d2c0c85e46396bdb218f6a79c00
Jiayi Tan <jitan@coh.org>
2019-07-15 06:57:22 -0700

    Update ET_test.R
    Add annotations

--- ET_test.R
+++ ET_test.R
@@ -46,7 +46,7 @@ colnames(ET)[2]="coefficient"
 ET$EntrezGene.ID<-annot.meta$EntrezGene.ID[match(ET$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
 ET$probe<-annot.meta$probe[match(ET$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
 
-cat("Run ET gene sig.score in BC samples\n")
+cat("Run ET gene sig.score in all patient samples\n")
 ET.score <- sig.score(x=ET, data=data.meta, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
 
 cat("Generate subtype signature score and survival data\n")
@@ -55,8 +55,10 @@ library(tibble)
 ET_score<-rownames_to_column(ET_score,var ="patient_id")
 colnames(ET_score)[2]="sig.score"
 
+cat("Generate exhuasted T cell sig. score in subtype\n")
 sub_score<-subset(ET_score, ET_score$patient_id %in% subtype_clin$pid)
 
+cat("Extract survival information from clinical data in subtype sig.score data frame")
 sub_score<-sub_score[order(-sub_score$sig.score),]
 sub_score$OS<-subtype_clin$T[match(subtype_clin$pid, sub_score$patient_id)]
 sub_score$os_status<-subtype_clin$DeathBreast[match(subtype_clin$pid, sub_score$patient_id)]
@@ -65,9 +67,8 @@ subtype_clin$dfs_status<-ifelse(c(subtype_clin$LR==0 & subtype_clin$DR==0),0,1)
 sub_score$DFS<-subtype_clin$DFS[match(sub_score$patient_id,subtype_clin$pid)]
 sub_score$dfs_status<-subtype_clin$dfs_status[match(sub_score$patient_id,subtype_clin$pid)]
 
-print(dim(sub_score))
 
-stop("JUST STOP!!!!!!!")
+cat("Select patient samples by 25/25 cutoff of exhuasted T sig.score")
 
 library(dplyr)
 top_25<-subset(sub_score, sig.score > quantile(sig.score, prob = 1 - 25/100))
@@ -75,9 +76,6 @@ bot_25<-subset(sub_score, sig.score < quantile(sig.score, prob = 25/100))
 top_25$score="t25%"
 bot_25$score="b25%"
 sub_score<-rbind(top_25,bot_25)
-
-
-
 
 
 filename<- paste("c:/Users/jitan/Documents/ET_", subtype, ".xlsx", sep = "")
