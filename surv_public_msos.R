488049e2a810a28cd70217f97b481986f8ce961e
weihuaguo <gwh120104@gmail.com>
2019-07-22 11:38:06 -0700

    Clean the codes and save the clinical data to RDS

--- surv_public_msos.R
+++ surv_public_msos.R
@@ -113,7 +113,7 @@ annot_file = "HumanHT_12_v30_R3_cleaned_v2.xlsx" # Microarray/Genome annotation
 }
 if (db_name == "tcga_brca") {
 expr_file = "tcga_brca_log2trans_fpkm_uq_v2.RDS" # Expression file
-clin_rds = "07212019_tcga_clinical_info.xlsx" # clinical information with merged disease-free survival
+clin_rds = "07212019_tcga_clinical_info.RDS" # clinical information with merged disease-free survival
 annot_file = "gencode.gene.info.v22.xlsx" # Microarray/Genome annotation
 }
 
@@ -146,7 +146,6 @@ st = Sys.time()
 expr = readRDS(paste(data_dir, expr_file, sep = ""))
 print(Sys.time()-st)
 # print(meta_expr[1:9,1:6]) # Check the input in terminal
-# data.meta<-t(meta_expr)
 
 cat("Loading clinical data...\n")
 st = Sys.time()
@@ -166,10 +165,11 @@ if (FALSE) {
 	cat("Save the clinical information to ", rds_file, "\n")
 	q(save = "no")
 }
-# clin_info = readRDS(paste(data_dir, clin_rds, sep = ""))
-clin_info = as.data.frame(read_excel(paste(data_dir, clin_rds, sep = "")))
-
+clin_info = readRDS(paste(data_dir, clin_rds, sep = ""))
+# clin_info = as.data.frame(read_excel(paste(data_dir, clin_rds, sep = "")))
+# saveRDS(clin_info, file = paste(data_dir, "07212019_tcga_clinical_info.RDS", sep = ""))
 print(Sys.time()-st)
+q(save = "no")
 
 cat("Start to filter by clinical info...\n")
 sub_clin = clin_info
@@ -237,25 +237,12 @@ if (dim(sub_clin)[1] <= 3) {stop("TOO SMALL SAMPLE SIZE!!!")}
 
 
 cat("Loading genome annotation data...\n")
-# library(readxl)
 annot.meta<-read_excel(paste(data_dir, annot_file, sep = ""))
 st = Sys.time()
 annot = as.data.frame(read_excel(paste(data_dir, annot_file, sep = "")))
 # annot = read.table(paste(data_dir, annot_file, sep = ""), header = TRUE)
 # print(head(annot))
 print(Sys.time()-st)
-
-## Save this for later
-if (FALSE) {
-annot.meta<-annot.meta[c(5,9,14)]
-colnames(annot.meta)[1]="NCBI.gene.symbol"
-colnames(annot.meta)[2]="EntrezGene.ID"
-colnames(annot.meta)[3]="probe"}
-cat("Load gene signature...\n")
-# library(xlsx)
-sign = read.table(paste(sign_dir, sign_file, sep = ""), header = TRUE, row.names = 1)
-# ET<-read.xlsx(file="trm_tex_brtissue_only_all_markers.xlsx",4, header = TRUE)
-# print(head(sign))
 
 ## PREP for sig.score
 # data = [r:sample, c:gene/probe]
@@ -415,104 +402,4 @@ survana(data = sub_scres, type = "os", plot = res_dir, gptype = "TRM sig.score",
 if (db_name != "tcga_brca") {
 survana(data = sub_scres, type = "rfs", plot = res_dir, gptype = "TRM sig.score", 
 	cox = res_dir, coxfac = c("age","tsize","node_stat"), gdoi = gdoi)}
-q(save = "no")
 
-# ET<-ET[c(1,3)]
-# colnames(ET)[1]="NCBI.gene.symbol"
-# colnames(ET)[2]="coefficient"
-# ET$EntrezGene.ID<-annot.meta$EntrezGene.ID[match(ET$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
-# ET$probe<-annot.meta$probe[match(ET$NCBI.gene.symbol, annot.meta$NCBI.gene.symbol)]
-
-# cat("Run ET gene sig.score in all patient samples\n")
-# ET.score <- sig.score(x=ET, data=data.meta, annot=annot.meta, do.mapping=FALSE, signed=TRUE, verbose=TRUE)
-
-# cat("Generate subtype signature score and survival data\n")
-# ET_score<- as.data.frame (ET.score$score)
-# library(tibble)
-# ET_score<-rownames_to_column(ET_score,var ="patient_id")
-# colnames(ET_score)[2]="sig.score"
-
-# cat("Generate exhuasted T cell sig. score in subtype\n")
-# sub_score<-subset(ET_score, ET_score$patient_id %in% subtype_clin$pid)
-
-# cat("Extract survival information from clinical data to subtype sig.score data frame")
-# sub_score<-sub_score[order(-sub_score$sig.score),]
-# sub_score$OS<-subtype_clin$T[match(subtype_clin$pid, sub_score$patient_id)]
-# sub_score$os_status<-subtype_clin$DeathBreast[match(subtype_clin$pid, sub_score$patient_id)]
-# subtype_clin$DFS<-ifelse(subtype_clin$TLR>subtype_clin$TDR,subtype_clin$TDR,subtype_clin$TLR)
-# subtype_clin$dfs_status<-ifelse(c(subtype_clin$LR==0 & subtype_clin$DR==0),0,1) 
-# sub_score$DFS<-subtype_clin$DFS[match(sub_score$patient_id,subtype_clin$pid)]
-# sub_score$dfs_status<-subtype_clin$dfs_status[match(sub_score$patient_id,subtype_clin$pid)]
-
-# stop("JUST STOP!!!!!!!")
-
-# cat("Select patient samples by 25/25 cutoff of exhuasted T sig.score")
-
-# library(dplyr)
-# top_25<-subset(sub_score, sig.score > quantile(sig.score, prob = 1 - 25/100))
-# bot_25<-subset(sub_score, sig.score < quantile(sig.score, prob = 25/100))
-# top_25$score="t25%"
-# bot_25$score="b25%"
-# sub_score<-rbind(top_25,bot_25)
-
-
-
-# filename<- paste("c:/Users/jitan/Documents/ET_", subtype, ".xlsx", sep = "")
-# library(xlsx)
-# write.xlsx(sub_score, filename)
-
-
-
-
-cat("Generate survival plot\n")
-library("survival")
-library("survminer")
-
-sub_score$os_status<-as.character(sub_score$os_status)
-sub_score$os_status<-as.numeric(sub_score$os_status)
-os_fit <- survfit(Surv(OS, os_status) ~ score, data = sub_score)
-
-
-os_plot<-ggsurvplot(os_fit,
-          pval = TRUE, conf.int = FALSE, ylab = "Overall survival",
-          risk.table = TRUE, # Add risk table
-          risk.table.col = "strata", # Change risk table color by groups
-          linetype = "strata", # Change line type by groups
-          surv.median.line = "hv", # Specify median survival
-          palette = c("#E7B800", "#2E9FDF"))
-
-filename<- paste("c:/Users/jitan/Documents/", subtype, "_ET_os.jpeg", sep = "")
-
-ggsave(file=filename, print(os_plot))
-
-
-dfs_fit <- survfit(Surv(DFS, dfs_status) ~ score, data = sub_score)
-dfs_plot <-ggsurvplot(dfs_fit,
-          pval = TRUE, conf.int = FALSE, ylab = "Disease-free survival",
-          risk.table = TRUE, # Add risk table
-          risk.table.col = "strata", # Change risk table color by groups
-          linetype = "strata", # Change line type by groups
-          surv.median.line = "hv", # Specify median survival
-          palette = c("#E7B800", "#2E9FDF"))
-
-filename<- paste("c:/Users/jitan/Documents/", subtype, "_ET_dfs.jpeg", sep = "")
-
-ggsave(file=filename, print(dfs_plot))
-
-
-sub_score$grade<-clin_info$grade[match(sub_score$patient_id, clin_info$pid)]
-sub_score$grade<-as.character(sub_score$grade)
-sub_score$grade<-as.numeric(sub_score$grade)
-sub_score<-na.omit(sub_score)
-
-sub_score$grade<-as.factor(sub_score$grade)
-
-cat("Perform ANOVA test by BC grade in BC subtype based on exhuasted T cell sig.score\n")
-p <- ggplot(sub_score, aes(x = grade, y = sig.score, color = grade))
-p + geom_point()+stat_compare_means(method = "anova") + theme_bw()
-
-cat("Perform t-test between each two groups based on BC grade\n")
-my_comparisons <- list( c("1", "2"), c("1", "3"), c("2", "3") )
-ggplot(sub_score, aes(x=grade, y=sig.score, color = grade)) + geom_point() + 
-    stat_compare_means(comparisons = my_comparisons)+ # Add pairwise comparisons p-value
-    stat_compare_means(label.y = 10) + theme_bw()
