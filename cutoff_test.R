4a3d29944708bf057db394d8c0fdec9b65e7fd78
joycetan817 <51905304+joycetan817@users.noreply.github.com>
2019-10-02 18:02:03 -0700

    add combine plot

--- cutoff_test.R
+++ cutoff_test.R
@@ -45,6 +45,66 @@ singene_expr = function (gene, expr, annot, subdf, caltype = "mean", map = TRUE)
 
 }
 
+
+cutoff_screen <- function (data, type, qcut, gp_app, gp_val) {
+	cat("Generate survival/cox pvalue from all cutoffs\n")
+	pval_cut<-as.data.frame(matrix(ncol=2, nrow=length(qcut)))
+	colnames(pval_cut)[1:2]= c("pval", "log_score")
+	rownames(pval_cut) = qcut
+
+	for (cut in 1: length(qcut)) {
+		temp_scres <- data
+		temp_scres$gpvalue <- temp_scres[,gp_val]
+		if (gp_app == "oneqcut") {
+			qcov = quantile(temp_scres$gpvalue, c(1-qcut[cut])) # one quantile cutoff
+			temp_scres$group = "Medium"
+			temp_scres[temp_scres$gpvalue <= qcov[[1]],"group"] = "Low"  
+			temp_scres[temp_scres$gpvalue > qcov[[1]],"group"] = "High"
+		}
+		if (gp_app == "symqcut") {
+			qcov = quantile(temp_scres$gpvalue, c(qcut[cut], 1-qcut[cut])) # symmetric quantile cutoff
+			temp_scres$group = "Medium"
+			temp_scres[temp_scres$gpvalue <= qcov[[1]],"group"] = "Low"
+			temp_scres[temp_scres$gpvalue >= qcov[[2]],"group"] = "High"
+			temp_scres = temp_scres[temp_scres$group != "Medium",]
+		}
+		if (type == "surv") {
+			fit <- survfit(Surv(ost, ose) ~ group, data = temp_scres)
+			pval_cut[cut, 1]=-log10(surv_pvalue(fit)[2])
+		} 
+		if (type == "cox_reg") {
+		res.cox <- coxph(Surv(ost, ose) ~ group, data=temp_scres)
+		prescox = summary(res.cox)
+		pval_cut[cut, 1]=-log10(prescox$sctest["pvalue"])
+		pval_cut[cut, 2]=prescox$sctest["test"]
+		}
+	}
+
+	if (type == "surv") {
+		pval_cut[2] = NULL
+	}
+	pval_csv = paste(res_dir, gp_val, "cut_pval.csv")
+	write.csv(pval_cut, file= pval_csv)
+################################################################################
+	pval_cut$cutoff <- as.numeric(rownames(pval_cut))
+	if( type == "cox_reg") {
+		c <- ggplot(pval_cut, aes(cutoff, log_score))
+		c <- c + geom_col() + labs(title = paste(gp_val, db_name, pamst), x ="cutoff", y = "log-rank test score") + coord_cartesian(xlim=c(0, 1)) + theme_classic()
+		ggsave(c, file = paste(res_dir, gp_val, "_logscore_barplot.tiff"),  dpi = 300, width = 9, height = 6, units = "in", device = "tiff")
+		
+		d <- ggplot(pval_cut, aes(cutoff, pval))
+		d <- d + geom_col() + labs(title = paste(gp_val, db_name, pamst), x ="cutoff", y = "adjust P value") + coord_cartesian(xlim=c(0, 1)) + theme_classic()
+		ggsave(d, file = paste(res_dir, gp_val, "_pval_barplot.tiff"),  dpi = 300, width = 9, height = 6, units = "in", device = "tiff")
+		
+	} else {
+		c <- ggplot(pval_cut, aes(cutoff, pval)) 
+		c <- c + geom_col() + labs(title = paste(gp_val, db_name, pamst), x ="cutoff", y = "adjust P value") + coord_cartesian(xlim=c(0, 1)) + theme_classic()
+		#c <- c + geom_vline(xintercept = c(0.25, 0.75), size = 0.8, colour = "red",linetype = "dotdash")
+		ggsave(c, file = paste(res_dir, gp_val, "_pval_barplot.tiff"),  dpi = 300, width = 9, height = 6, units = "in", device = "tiff")
+	}
+	return(c) 
+}
+
 suppressMessages(library(genefu))
 suppressMessages(library(readxl))
 suppressMessages(library(ggplot2))
@@ -53,11 +113,11 @@ suppressMessages(library(survminer))
 
 
 work_dir = "//Bri-net/citi/Peter Lee Group/Weihua/surv_validation/"
-db_name = "metabric"
-#db_name = "tcga_brca"
+#db_name = "metabric"
+db_name = "tcga_brca"
 sg_name = "tex_brtissue" # Colt's Tex sig from breast tissue c2
 pval_name = "cut_pval"
-expr_type = "ilid" # ilid: raw data from EGA, median: raw median data from cbioportal, medianz: zscore from cbioportal
+expr_type = "" # ilid: raw data from EGA, median: raw median data from cbioportal, medianz: zscore from cbioportal
 selfmap = TRUE # NOTE: ilid/tcga requires this as TRUE; median as FALSE
 
 if (db_name == "metabric") {
@@ -82,29 +142,30 @@ sign_file = "tex_signature_colt_c2.txt" # Signature file Colt's Tex
 histype = "IDC" # histology type: IDC/DCIS
 pamst = c("LumA","LumB") # PAM50 status: LumA/LumB/Basal/Normal/Her2
 hrtype = "" #c("N", "N", "N") # N: Negative, P: Positive, "-": DON'T CARE
-gp_gene = ""
-gptype = "Tex sig.score"
-qcut <- seq(from = 0.05, to = 0.95, by = 0.001) # 
-gp_app = "symqcut"#"symqcut" # oneqcut: one quantile cutoff (upper percential), symqcut: symmetric quantile cutoff
-pval = "surv" #  p value get from "surv" or "cox_reg"
+gp_gene = "CD8A"
+gptype = "CD8A"
+qcut <- seq(from = 0.05, to = 0.95, by = 0.001) # screen cutoff range
+gp_app = "oneqcut"#"symqcut" # oneqcut: one quantile cutoff (upper percential), symqcut: symmetric quantile cutoff
+pval = "cox_reg" #  p value get from "surv" or "cox_reg"
+gp_val = c("sig_score", "CD8A")
 
-res_folder = "Tex_sym_surv_LumA_B_IDC_ega" # NOTE: Please change this folder name to identify your experiments
+res_folder = "CD8A_cox_LumA_B_IDC_tcga_corr" # NOTE: Please change this folder name to identify your experiments
 res_dir = paste(pval_dir, res_folder, "/", sep ="")
 dir.create(file.path(pval_dir, res_folder), showWarnings = FALSE)
 
 cat("Loading expression data...\n")
 st = Sys.time()
 #expr = readRDS(paste(data_dir, expr_file, sep = ""))
-expr = readRDS("metabric_expr_ilid.RDS")
+#expr = readRDS("metabric_expr_ilid.RDS")
 #expr = readRDS("data_expression_median.RDS") # When test the script using cBioportal
-#expr = readRDS("tcga_brca_log2trans_fpkm_uq_v2.RDS") # When test the script using tcga
+expr = readRDS("tcga_brca_log2trans_fpkm_uq_v2.RDS") # When test the script using tcga
 print(Sys.time()-st)
 expr<-as.data.frame(expr)
 
 cat("Loading clinical data...\n")
 #clin_info = readRDS(paste(data_dir, clin_rds, sep = ""))
-clin_info = readRDS("merge_clin_info_v3.RDS")
-#clin_info = read_excel("08272019_tcga_pam50_clin.xlsx", sheet = 1)
+#clin_info = readRDS("merge_clin_info_v3.RDS")
+clin_info = read_excel("08272019_tcga_pam50_clin.xlsx", sheet = 1)
 
 
 
@@ -267,63 +328,30 @@ cat("Extract survival and treatment information from clinical data to subtype si
 sub_scres$ost = sub_clin$T[match(sub_scres$pid, sub_clin$pid)]
 sub_scres$ose = as.numeric(sub_clin$DeathBreast[match(sub_scres$pid, sub_clin$pid)])
 ## Add all the other factors
-sub_scres$age = as.numeric(sub_clin$age_at_diagnosis[match(sub_scres$pid, sub_clin$pid)])
-sub_scres$grade = as.numeric(sub_clin$grade[match(sub_scres$pid, sub_clin$pid)])
-sub_scres$tsize = as.numeric(sub_clin$tumor_size[match(sub_scres$pid, sub_clin$pid)])
-sub_scres$node_stat = as.numeric(sub_clin$Lymph.Nodes.Positive[match(sub_scres$pid, sub_clin$pid)])
+#sub_scres$age = as.numeric(sub_clin$age_at_diagnosis[match(sub_scres$pid, sub_clin$pid)])
+#sub_scres$grade = as.numeric(sub_clin$grade[match(sub_scres$pid, sub_clin$pid)])
+#sub_scres$tsize = as.numeric(sub_clin$tumor_size[match(sub_scres$pid, sub_clin$pid)])
+#sub_scres$node_stat = as.numeric(sub_clin$Lymph.Nodes.Positive[match(sub_scres$pid, sub_clin$pid)])
 
 
 #################################################################################
 
-pval_cut<-as.data.frame(matrix(ncol=1, nrow=length(qcut)))
-colnames(pval_cut)[1]= "padj"
-rownames(pval_cut) = qcut
+tex_plot <- cutoff_screen(data = sub_scres, type = pval, qcut = qcut, gp_app = gp_app, gp_val = gp_val[1])
+gene_plot <- cutoff_screen(data = sub_scres, type = pval, qcut = qcut, gp_app = gp_app, gp_val = gp_val[2])
 
-cat("Generate survival/cox pvalue from all cutoffs\n")
+corr_plot <- ggscatter(sub_scres, x = "sig_score", y = "CD8A", # genes correlate with sig.score
+           color = "gray40", shape = 19, size = 1.5,
+           add = "reg.line",  # Add regressin line
+           add.params = list(color = "black", fill = "lightgray"), # Customize reg. line
+           conf.int = TRUE, # Add confidence interval
+           cor.coef = TRUE, # Add correlation coefficient.
+           cor.coeff.args = list(method = "spearman", label.x = 3, label.sep = "\n")) + border() + theme(plot.margin = margin(0, 0, 0, 0, "pt"))
 
-for (cut in 1: length(qcut)) {
-	temp_scres <- sub_scres
-	if (gp_app == "oneqcut") {
-		qcov = quantile(temp_scres$gpvalue, c(1-qcut[cut])) # one quantile cutoff
-		temp_scres$group = "Medium"
-		temp_scres[temp_scres$gpvalue <= qcov[[1]],"group"] = "Low"  
-		temp_scres[temp_scres$gpvalue > qcov[[1]],"group"] = "High"
-	}
-	if (gp_app == "symqcut") {
-		qcov = quantile(temp_scres$gpvalue, c(qcut[cut], 1-qcut[cut])) # symmetric quantile cutoff
-		temp_scres$group = "Medium"
-		temp_scres[temp_scres$gpvalue <= qcov[[1]],"group"] = "Low"
-		temp_scres[temp_scres$gpvalue >= qcov[[2]],"group"] = "High"
-		temp_scres = temp_scres[temp_scres$group != "Medium",]
-	}
-	if (pval == "surv") {
-		fit <- survfit(Surv(ost, ose) ~ group, data=temp_scres)
-		pval_cut[cut, 1]=-log10(surv_pvalue(fit)[2])
-	} 
-	if (pval == "cox_reg") {
-		if(db_name == "tcga_brca") {
-			res.cox <- coxph(Surv(ost, ose) ~ group+age+grade+node_stat, data=temp_scres)
-			} else {
-			res.cox <- coxph(Surv(ost, ose) ~ group+age+grade+tsize+node_stat, data=temp_scres)
-			}
-		prescox = summary(res.cox)
-		pval_cut[cut, 1]=-log10(prescox$sctest["pvalue"])
-	}
-}
-
-pval_csv = paste(res_dir, "cut_pval.csv")
-write.csv(pval_cut, file= pval_csv)
-
-bar_tiff = paste(res_dir, "barplot.tiff")
-tiff(bar_tiff, res=300, width=12, heigh=9, units='in')
-plot_pval <- pval_cut[1:length(qcut), "padj"]
-if( pval == "cox_reg") {
-	pval_cut <- as.data.frame(pval_cut[order(pval_cut$padj),])
-	min <- min(pval_cut)
-	max <- max(pval_cut)
-	barplot(plot_pval, main = paste(gptype, pval, "cutoff"), xlab = "cutoff", ylab = "padj", ylim = c(min,max))
-} else {
-	barplot(plot_pval, main = paste(gptype, pval, "cutoff"), xlab = "cutoff", ylab = "padj")
-}
-dev.off()
-
+tex_plot<- tex_plot + clean_theme() +labs(title = NULL)
+gene_plot <- gene_plot + clean_theme () + labs(title = NULL) + rotate()
+combine_plot<-ggarrange(tex_plot, NULL, corr_plot, gene_plot, 
+          ncol = 2, nrow = 2,  align = "hv", 
+          widths = c(2, 1), heights = c(1, 2),
+          common.legend = TRUE)
+combine_tiff <- paste(res_dir, gp_val[1], gp_val[2], "combine_plot.tiff", sep = "_")
+ggsave(combine_plot, file = combine_tiff, dpi = 300, width = 11, height = 10, units = "in", device = "tiff" )
