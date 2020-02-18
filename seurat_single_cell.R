c53e16d240004bd67e88d5cb70be0a40d0372ed6
joycetan817 <51905304+joycetan817@users.noreply.github.com>
2020-02-18 11:35:08 -0800

    Update seurat_single_cell.R

--- seurat_single_cell.R
+++ seurat_single_cell.R
@@ -7,8 +7,8 @@ rm(list = ls(all.names = TRUE))
 batch_corr = function (sc_obj, group) {
 	sample.list <- SplitObject(sc_obj, split.by = group)
 	for (i in 1:length(sample.list)) {
-    sample.list[[i]] <- FindVariableFeatures(sample.list[[i]], selection.method = "vst", nfeatures = 2000, verbose = FALSE)
-	}
+	    sample.list[[i]] <- FindVariableFeatures(sample.list[[i]], selection.method = "vst", nfeatures = 2000, verbose = FALSE)
+		}
 
 	reference.list <- sample.list
 	integrate.anchors <- FindIntegrationAnchors(object.list = reference.list, dims = 1:30)
@@ -17,9 +17,56 @@ batch_corr = function (sc_obj, group) {
 	return (sc_cor)
 }
 
-sr_clst = function(sc_obj, ndim, resdir) {
-	sc_obj <- ScaleData(sc_obj)
-	sc_obj <- RunPCA(sc_obj)
+sr_QC = function (sc_obj, feaRNA_min, feaRNA_max, perMT, resdir, proFlag) {
+	sc_obj[["percent.mt"]] <- PercentageFeatureSet(sc_obj, pattern = "^MT-")
+	sc_obj@meta.data$percent.mt[is.na(sc_obj@meta.data$percent.mt)] <- 0
+	featplot <- VlnPlot(sc_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+	#if (class(featplot) == "try-error") {tmp_mt <- sc_obj@meta.data[["percent.mt"]]
+		#tmp_mt[is.na(tmp_mt)] = 0
+		#sc_obj@meta.data[["percent.mt"]] <- tmp_mt
+		#featplot <- VlnPlot(sc_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+		#}
+	ggsave(plot=featplot, file = paste(res_dir, "feature_plot_bf_filter.tiff", sep = ""),  width = 9, height = 6)
+	plot1 <- FeatureScatter(sc_obj, feature1 = "nCount_RNA", feature2 = "percent.mt")
+	plot2 <- FeatureScatter(sc_obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+	featscat <- CombinePlots(plots = list(plot1, plot2))
+	ggsave(plot = featscat, file = paste(res_dir, "feature_scatter_bf_filter.tiff", sep = ""),  width = 9, height = 6)
+
+
+	sc_obj <- subset(sc_obj, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 10) ###check feature plot
+	featplot_filt <- VlnPlot(sc_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+	ggsave(plot = featplot_filt, file = paste(res_dir, "feature_plot_aft_filter.tiff", sep = ""),  width = 9, height = 6)
+
+	####Normalizing the data
+	sc_obj <- NormalizeData(sc_obj)
+	if (proFlag) {sc_obj = NormalizeData(sc_obj, assay = "Protein", normalization.method = "CLR")}
+
+	sc_obj <- FindVariableFeatures(sc_obj, selection.method = "vst", nfeatures = 2000)	
+	top10 <- head(VariableFeatures(sc_obj), 10)
+	# plot variable features with and without labels
+	plot1 <- VariableFeaturePlot(sc_obj)
+	plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+	ggsave(plot = plot2, filename = paste(res_dir, "vari_feature.tiff", sep = ""),  width = 7, heigh = 6)
+	return (sc_obj)
+
+}
+
+sr_clst = function(sc_obj, ndim, resdir, proFlag) {
+
+	#sc_obj <- FindVariableFeatures(sc_obj, selection.method = "vst", nfeatures = 2000)	
+	#top10 <- head(VariableFeatures(sc_obj), 10)
+	 #plot variable features with and without labels
+	#plot1 <- VariableFeaturePlot(sc_obj)
+	#plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+	#ggsave(plot = plot2, filename = paste(res_dir, "vari_feature.tiff", sep = ""),  width = 7, heigh = 6)
+
+	
+
+	allGenes = rownames(sc_obj[["RNA"]])
+	sc_obj = ScaleData(sc_obj, features = allGenes, assay = "RNA")
+	if (proFlag) {sc_obj = ScaleData(sc_obj, assay = "Protein")}
+	
+	sc_obj <- RunPCA(sc_obj, features = VariableFeatures(object = sc_obj))
 	pca_vizdim = VizDimLoadings(sc_obj, dim = 1:6, reduction = "pca")
 	ggsave(plot = pca_vizdim, filename = paste(resdir, "pca_vizdim_plot.tiff", sep = ""), width = 10, height = 15)
 	
@@ -49,11 +96,16 @@ sr_clst = function(sc_obj, ndim, resdir) {
 	btc = DimPlot(sc_obj, reduction = "umap", group.by = "orig.ident")
 	ggsave(plot = btc, filename = paste(resdir, "pca", ndim, "_batch_check_aft_cor.tiff", sep = ""), width = 9, heigh = 6)
 
+	expr = c("PTPRC", "EPCAM", "ATL1", "CD3D", "CD8A", "CD4", "PDCD1", "TCF7", "NKG7", "MS4A1", "CD27", "CD19", 
+	             "CD68", "FCER1A", "CD14", "ITGAX", "IL3RA", "CD63", "NCAM1", "TPSAB1", "GNLY", "FOXP3", "TBX21", "GATA3")
 
 	expr = c("PTPRC", "CD3E", "CD4", "CD8A", "TCF7", "IL7R", "ITGAE", "PDCD1", "ENTPD1", "DPP4", "FOXP3")
 	expr_ol = FeaturePlot(sc_obj, features = expr, reduction = "umap",
 					    min.cutoff = "q05", max.cutoff = "q95", ncol = 4, cols = c("orange1", "grey", "purple"))
-	ggsave(plot = expr_ol, filename = paste(resdir, "expression_overlay.tiff", sep = ""),  width = 16, heigh = 10)
+	ggsave(plot = expr_ol, filename = paste(resdir, "pca", ndim ,"expression_overlay_cor.tiff", sep = ""),  width = 16, heigh = 10)
+
+	vln_feat = VlnPlot(sc_obj, features = expr, slot = "counts", log = TRUE, ncol = 3, pt.size = 0.5)
+	ggsave(plot = vln_feat, filename = paste(resdir, "pca", ndim, "violin_gene_feature_cor.tiff", sep = ""), width = 12, heigh = 16)
 
 	cluster.markers <- FindAllMarkers(sc_obj, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 	write.csv(cluster.markers, paste(resdir, "pca", ndim, "_cluster_marker.csv", sep = ""))
@@ -63,26 +115,29 @@ sr_clst = function(sc_obj, ndim, resdir) {
 
 ######################################################
 suppressMessages(library(dplyr))
+suppressMessages(library(tidyr))
 suppressMessages(library(Seurat))
 suppressMessages(library(ggplot2))
+suppressMessages(library(stringr))
 
 
-data_dir = "//isi-dcnl/user_data/plee/Group/scmodb/10X/"
 work_dir = "//bri-net/citi/Peter Lee Group/Joyce/single_cell/seurat_analysis/"
-data_name = "public_br_scRNAseq" #Public scRNA raw data
+sample_type = "vdj_citeseq_expr_112019"#"10X" #"public_br_scRNAseq" #Public scRNA raw data
  #"vdj_citeseq_expr_112019" / "10X"
+data_name = "394_results"
 cluster = "immune_res" # all immune cells 
 reclst = "tcell_res" #recluster in t cells
 
-scobj_dir = paste(data_dir, data_name, "/", sep = "") 
-res_dir = paste(work_dir, data_name, cluster, "/", sep = "/")
-reclst_res_dir = paste(work_dir, data_name, reclst, "/", sep = "/")
+data_dir = paste(work_dir, sample_type, "/", sep = "") 
+res_dir = paste(work_dir, data_name, "/", cluster, "/", sep = "")
+reclst_res_dir = paste(work_dir, data_name, "/", reclst, "/", sep = "")
 
 
 
 batch_check = TRUE #if batch check is needed
 batch_cor = TRUE #if batch correction is needed
 recluster = TRUE
+proteinflag = TRUE
 
 
 
@@ -97,31 +152,81 @@ sc_obj <- CreateSeuratObject(counts = sc_obj, project = "sc_BC")
 saveRDS(sc_obj, paste(res_dir, "all_filter_seurat_project.RDS", sep = ""))
 
 
-sc_obj <- readRDS(paste(scobj_dir, "BC3_5_TNBC_all_filter_seurat_project.RDS", sep= ""))
-sc_obj <- readRDS(paste(scobj_dir, "all_raw_10X_seurat_objects_list.RDS", sep = ""))
-sc_obj <- read.csv(paste(work_dir, data_name, "raw_corrected.csv", sep= "/"))
+if (sample_type == "public_br_scRNAseq") {
+count_mat <- readRDS(paste(data_dir, "BC3_5_TNBC_all_filter_seurat_project.RDS", sep= ""))
+#count_mat <- readRDS(paste(data_dir, "ER_p1246_matrix.RDS", sep= ""))
+#count_mat <- readRDS(paste(data_dir, "tumor_matrix.RDS", sep= ""))
+#count_mat <- readRDS(paste(data_dir, "all_BC_matrix.RDS", sep= ""))
+}
+sc_obj <- CreateSeuratObject(counts = count_mat, project = data_name)
 
+if (sample_type == "vdj_citeseq_expr_112019") {
+	inFiles = list.files(data_dir, pattern = "_raw_feature_bc_matrix")
+	saName = str_split_fixed(inFiles, "_raw", n = 2)[,1]
+
+	srData = vector(mode = "list", length = length(saName))
+	srObjs = vector(mode = "list", length = length(saName))
+	names(srData) = saName
+	names(srObjs) = saName
+
+	for (isa in 1:length(inFiles)) {
+		st = Sys.time()
+		message(saName[isa], ":" , inFiles[isa])
+		tmpDir = paste(data_dir, inFiles[isa], sep = "")
+		tmpData = try(Read10X(tmpDir))
+		
+		if (proteinflag) {
+			oldRowName = rownames(tmpData$`Antibody Capture`)
+			newRowName = str_split_fixed(oldRowName, "_", n = 4)[,3]
+			rownames(tmpData$`Antibody Capture`) = newRowName
+			srData[saName[isa]] = tmpData
+
+			cat("Protein data is available!\n")
+			tmpObj = CreateSeuratObject(counts = tmpData$`Gene Expression`, project = saName[isa])
+			tmpObj[["Protein"]] = CreateAssayObject(counts = tmpData$`Antibody Capture`)
+		}
+
+		tisName = str_split_fixed(saName[isa], "_", n = 3)[,3]
+		patName = str_split_fixed(saName[isa], "_", n = 3)[,1]
+		cat(tisName, patName, "\n")
+		tmpObj@meta.data$tissue = tisName
+		tmpObj@meta.data$patient = patName
+
+		#srObjs[saName[isa]] = tmpObj
+		#srObjs[isa] = tmpObj
+
+		#print(tmpObj)
+		tmpObj = sr_QC(tmpObj, feaRNA_min = 200, feaRNA_max = 2500, perMT = 10, resdir = res_dir, proFlag = proteinflag)
+		srObjs[[isa]] = tmpObj
+	}
+
+	integrate.anchors <- FindIntegrationAnchors(object.list = srObjs, dims = 1:30)
+	inteObjs <- IntegrateData(anchorset = integrate.anchors, dims = 1:30)
+	DefaultAssay(inteObjs) <- "integrated"
+
+
+
+if (batch_cor) {
+	integrateList = list("BC377" = bc377_xx_TUMOR,
+			     "BC389" = bc389_xx_TUMOR, 
+			     "BC392" = bc392_xx_TUMOR,
+			     "BC393" = bc393_xx_TUMOR
+			     )}
+
+
+}
+
+
+sc_obj <- readRDS(paste(data_dir, "all_raw_10X_seurat_objects_list.RDS", sep = ""))
+}
+if (sample_type == "10X") {
+sc_obj <- readRDS(paste(data_dir, "all_filter_10X_seurat_objects_tumor4_list.RDS", sep = ""))
+}
 
 
 
 ###########QC
-sc_obj[["percent.mt"]] <- PercentageFeatureSet(sc_obj, pattern = "^MT.")
-featplot <- VlnPlot(sc_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-ggsave(plot=featplot, file = paste(res_dir, "feature_plot_bf_filter.tiff", sep = ""),  width = 9, height = 6)
-plot1 <- FeatureScatter(sc_obj, feature1 = "nCount_RNA", feature2 = "percent.mt")
-plot2 <- FeatureScatter(sc_obj, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-featscat <- CombinePlots(plots = list(plot1, plot2))
-ggsave(plot = featscat, file = paste(res_dir, "feature_scatter_bf_filter.tiff", sep = ""),  width = 9, height = 6)
 
-
-sc_obj <- subset(sc_obj, subset = nFeature_RNA > 200 & nFeature_RNA < 2000 & percent.mt < 15) ###check feature plot
-featplot_filt <- VlnPlot(sc_obj, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
-ggsave(plot = featplot_filt, file = paste(res_dir, "feature_plot_aft_filter.tiff", sep = ""),  width = 9, height = 6)
-
-
-
-####Normalizing the data
-sc_obj <- NormalizeData(sc_obj)
 
 if (batch_check) {
 	btc <- sc_obj
@@ -147,6 +252,8 @@ if (recluster) {
 	tc <- subset(sc_cor, ident = c(0,4,5,6,8)) ###recluster t cell based on CD3 and CD45
 	sub_check <- DimPlot(tc)
 	ggsave(plot = sub_check, filename = paste(reclst_res_dir, "subset_check.tiff", sep = ""),  width = 9, heigh = 6)
+	btc_check <- DimPlot(tc, group.by = "orig.ident")
+	ggsave(plot = btc_check, filename = paste(reclst_res_dir, "subset_tcell_b_check.tiff", sep = ""),  width = 9, heigh = 6)
 	saveRDS(tc, paste(reclst_res_dir, "seurat_obj_tcell_cluster.RDS", sep = ""))
 
 if (batch_cor) {
@@ -154,8 +261,15 @@ if (batch_cor) {
 	tc_cor = sr_clst(tc_cor, ndim = 15, resdir = reclst_res_dir)
 }
 
+	tc <- FindVariableFeatures(tc, selection.method = "vst", nfeatures = 2000) # nfeatures = (1000,2000)
+	top10 <- head(VariableFeatures(tc), 10)
+	plot1 <- VariableFeaturePlot(tc)
+	plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+	ggsave(plot = plot2, file = paste(reclst_res_dir, "varfeat_plot.tiff", sep = ""), width = 9, height = 6)
 
 		tc_cor <- ScaleData(tc_cor)
+		
+		
 		tc_cor <- RunPCA(tc_cor)
 		pca_vizdim = VizDimLoadings(tc_cor, dim = 1:6, reduction = "pca")
 		ggsave(plot = pca_vizdim, filename = paste(reclst_res_dir, "pca_vizdim_plot_cor.tiff", sep = ""), width = 10, height = 15)
@@ -184,22 +298,22 @@ if (batch_cor) {
 		cls_umap = DimPlot(tc_cor, reduction = "umap")
 		ggsave(plot = cls_umap, filename = paste(reclst_res_dir, "pca", ndim, "_cluster_umap_plot_cor.tiff", sep = ""), width = 9, heigh = 6)
 		btc = DimPlot(tc_cor, reduction = "umap", group.by = "orig.ident")
-		ggsave(plot = btc, filename = paste(reclst_res_dir, ndim, "batch_check_aft_cor.tiff", sep = ""), width = 9, heigh = 6)
+		ggsave(plot = btc, filename = paste(reclst_res_dir, "pca", ndim, "batch_check_aft_cor.tiff", sep = ""), width = 9, heigh = 6)
 
 
 		expr = c("PTPRC", "CD3E", "CD4", "CD8A", "TCF7", "IL7R", "ITGAE", "PDCD1", "ENTPD1", "DPP4", "FOXP3")
 		expr_ol = FeaturePlot(tc_cor, features = expr, reduction = "umap",
 						    min.cutoff = "q05", max.cutoff = "q95", ncol = 4, cols = c("orange1", "grey", "purple"))
-		ggsave(plot = expr_ol, filename = paste(reclst_res_dir, "expression_overlay_cor.tiff", sep = ""),  width = 16, heigh = 10)
+		ggsave(plot = expr_ol, filename = paste(reclst_res_dir, "pca", ndim, "expression_overlay_cor.tiff", sep = ""),  width = 16, heigh = 10)
 
 		stem_expr = c("ZNF683", "CTSW", "LEF1", "NUCB2", "SCML4", "TXK", "CD55", "S100B", "MAL", "PLAC8")
 		stem_ol = FeaturePlot(tc_cor, features = stem_expr, reduction = "umap",
 						    min.cutoff = "q05", max.cutoff = "q95", ncol = 4, cols = c("orange1", "grey", "purple"))
-		ggsave(plot = stem_ol, filename = paste(reclst_res_dir, "stem_expression_overlay_cor.tiff", sep = ""),  width = 16, heigh = 10)
+		ggsave(plot = stem_ol, filename = paste(reclst_res_dir, "pca", ndim, "stem_expression_overlay_cor.tiff", sep = ""),  width = 16, heigh = 10)
 
 
 		vln_feat = VlnPlot(tc_cor, features = expr, slot = "counts", log = TRUE, ncol = 3, pt.size = 0.5)
-		ggsave(plot = vln_feat, filename = paste(reclst_res_dir, "violin_gene_feature_cor.tiff", sep = ""), width = 12, heigh = 16)
+		ggsave(plot = vln_feat, filename = paste(reclst_res_dir, "pca", ndim, "violin_gene_feature_cor.tiff", sep = ""), width = 12, heigh = 16)
 		cluster.markers <- FindAllMarkers(tc_cor, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
 		write.csv(cluster.markers, paste(reclst_res_dir, "pca", ndim, "_cluster_marker_cor.csv", sep = ""))
 
